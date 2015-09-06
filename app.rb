8a8ba84803c606ef3380b8d19577d4cc2ebaf840
syon <syon.xiii@gmail.com>
2015-09-06 15:48:24 +0900

    feed url into hash

--- app.rb
+++ app.rb
@@ -18,14 +18,21 @@ $user_rules = {
   }
 }
 
+$user_feeds = {
+  '1441518694' => {feed_url: 'http://b.hatena.ne.jp/hotentry.rss'},
+  '1441518830' => {feed_url: 'http://japanese.engadget.com/rss.xml'},
+  '1441521964' => {feed_url: 'http://qiita.com/tags/Docker/feed.atom'}
+}
+
 get '/' do
   @say = "Hello World."
   slim :index
 end
 
-get '/feed' do
+get '/feed/:feed_id' do
+  feed = $user_feeds[params[:feed_id]]
   begin
-    feed_url = params[:url]
+    feed_url = feed[:feed_url]
     raise unless URI.regexp(%w(http https)) =~ feed_url
 
     ff = FeedFilter.new(feed_url, $user_rules, {debug: true})
@@ -33,7 +40,7 @@ get '/feed' do
     content_type :"application/xml; charset=#{ff.charset}"
     content
   rescue => e
-    return "Error"
+    return "Error" + e.to_s
   end
 end
 
