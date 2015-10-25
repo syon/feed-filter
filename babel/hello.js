826617edc742898c4191805fc9ea26c85f7265d3
syon <syon.xiii@gmail.com>
2015-10-25 21:54:19 +0900

    apply React sample

--- babel/hello.js
+++ babel/hello.js
@@ -1,4 +1,64 @@
+
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+      </div>
+    );
+  }
+});
+
+var CommentBox = React.createClass({
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+  },
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList data={this.state.data} />
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment key={comment.key} author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <CommentBox url="/sample.json" />,
   document.getElementById('hello')
 );
--- public/hello.js
+++ public/hello.js
@@ -1,7 +1,74 @@
-'use strict';
+"use strict";
 
-ReactDOM.render(React.createElement(
-  'h1',
-  null,
-  'Hello, world!'
-), document.getElementById('hello'));
+var Comment = React.createClass({
+  displayName: "Comment",
+
+  render: function render() {
+    return React.createElement(
+      "div",
+      { className: "comment" },
+      React.createElement(
+        "h2",
+        { className: "commentAuthor" },
+        this.props.author
+      )
+    );
+  }
+});
+
+var CommentBox = React.createClass({
+  displayName: "CommentBox",
+
+  loadCommentsFromServer: function loadCommentsFromServer() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: (function (data) {
+        this.setState({ data: data });
+      }).bind(this),
+      error: (function (xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }).bind(this)
+    });
+  },
+  getInitialState: function getInitialState() {
+    return { data: [] };
+  },
+  componentDidMount: function componentDidMount() {
+    this.loadCommentsFromServer();
+  },
+  render: function render() {
+    return React.createElement(
+      "div",
+      { className: "commentBox" },
+      React.createElement(
+        "h1",
+        null,
+        "Comments"
+      ),
+      React.createElement(CommentList, { data: this.state.data })
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  displayName: "CommentList",
+
+  render: function render() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return React.createElement(
+        Comment,
+        { key: comment.key, author: comment.author },
+        comment.text
+      );
+    });
+    return React.createElement(
+      "div",
+      { className: "commentList" },
+      commentNodes
+    );
+  }
+});
+
+ReactDOM.render(React.createElement(CommentBox, { url: "/sample.json" }), document.getElementById('hello'));
