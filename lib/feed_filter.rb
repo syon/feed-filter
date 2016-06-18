c89025ec16fe042bf094e6372faf3bc1ac239417
syon <syon.xiii@gmail.com>
2016-06-18 12:10:13 +0900

    :bug: guard when fetch missed

--- lib/feed_filter.rb
+++ lib/feed_filter.rb
@@ -11,8 +11,10 @@ class FeedFilter
 
   def fetch_feed(feed_id)
     @feed = Feeds.where(:feed_id => feed_id.to_i).first
-    @feed.fetched_at = Time.now
-    @feed.save!
+    if @feed
+      @feed.fetched_at = Time.now
+      @feed.save!
+    end
     @feed
   end
 
